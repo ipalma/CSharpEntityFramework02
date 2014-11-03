1e3d93283c1a323ffe38d6f012df8d36164ba2ef
ipalma <isaac.palma@tajamar365.com>
2014-11-03 14:31:50 +0100

    Ver 0.1 beta

---
+++ GestionRRHH/AppGestionRRHH.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestionRRHH.Modelo;
+
+namespace GestionRRHH
+{
+    class AppGestionRrhh
+    {
+        static GestionRRHHEntities db = new GestionRRHHEntities();
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+        /*public static IEnumerable<Empleado> GetEmpleadoById(int idEmp)
+        {
+            var datos = db.Empleado.Where(o => o.idEmpleado == idEmp).OrderBy(o => o.nombre);
+
+            return datos;
+        }
+
+        public static IEnumerable<Cargo> GetCargoById(int idCarg)
+        {
+            var datos = db.Cargo.Where(o => o.idCargo == idCarg);
+
+            return datos;
+        } */
+
+
+
+
+    }
+}
